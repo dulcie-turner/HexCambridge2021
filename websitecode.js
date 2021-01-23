be4e9223829b2b43282000360133a027c0da0c89
Dulcie Turner <dulciejturner@gmail.com>
2021-01-23 15:35:02 +0000

    More work done on the tasklist

--- websitecode.js
+++ websitecode.js
@@ -1,8 +1,16 @@
 function addTask() {
     var task = document.forms["newTask"]["task"].value;
-    var table = document.getElementById("taskList");
+    var time = document.forms["newTask"]["time"].value;
+
+    var table = document.getElementById("taskTable");
     var row = table.insertRow(-1);
+
     var taskName = row.insertCell(0);
     taskName.innerHTML = task;
+
+    var taskTime = row.insertCell(1);
+    taskTime.innerHTML = time;
+
     document.forms["newTask"]["task"].value = "";
+    document.forms["newTask"]["time"].value = "";
   } 
